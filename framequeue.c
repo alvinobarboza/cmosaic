cd3588618f4a207af48b7b538e933c038874fea9
Alvino Barboza <alvinobarboza@gmail.com>
2025-08-25 18:03:01 -0300

    First frame queue implementation

--- framequeue.c
+++ framequeue.c
@@ -1,16 +1,72 @@
-#include <inttypes.h>
+#define MAX_FRAME_QUEUE 15
+#define MIN_FRAME_QUEUE 3
 
-#define MAX_FRAME_BUFFER 15
-
-typedef struct framequeue
-{
+typedef struct FrameQueue {
     uint8_t capacity;
     uint8_t head;
     uint8_t tail;
     uint8_t size;
-    uint8_t *queue[MAX_FRAME_BUFFER];
-} Framequeue;
+    uint8_t **queue;
+    uint32_t frame_size;
+} FrameQueue;
 
-Framequeue new_framequeue(){
+FrameQueue *framequeue_new(uint8_t buffer_size, uint32_t frame_size) {
+    buffer_size = buffer_size < MIN_FRAME_QUEUE ? MIN_FRAME_QUEUE : buffer_size;
 
+    FrameQueue *fm = malloc(sizeof(FrameQueue));
+    fm->capacity = buffer_size;
+    fm->head = 0;
+    fm->tail = 0;
+    fm->size = 0;
+    fm->frame_size = frame_size;
+
+    fm->queue = malloc(sizeof(uint8_t)*buffer_size);
+
+    for (int i = 0; i < fm->capacity; i++)
+    {
+        fm->queue[i] = malloc(sizeof(uint8_t)*fm->frame_size);
+    }
+
+    return fm;
+}
+
+void framequeue_free(FrameQueue *fm) {
+    if (fm != NULL) 
+    {
+        for (int i = 0; i < fm->capacity; i++)
+        {
+            free(fm->queue[i]);
+        }
+        free(fm->queue);
+        free(fm);
+    }
+}
+
+void framequeue_enqueue(FrameQueue *fm, uint8_t *data) {
+    memcpy(fm->queue[fm->tail], data, fm->frame_size);
+
+    fm->tail = (fm->tail + 1) % fm->capacity;
+
+    if (fm->size == fm->capacity)
+    {
+        fm->head = (fm->head + 1) % fm->capacity;
+        return;
+    }
+
+    fm->size += 1;
+}
+
+uint8_t *framequeue_dequeue(FrameQueue *fm) {
+    if (fm->size == 0)
+    {
+        return NULL;
+    }
+
+    uint8_t *data = malloc(sizeof(uint8_t)*fm->frame_size);
+    memcpy(data, fm->queue[fm->head], fm->frame_size);
+
+    fm->head = (fm->head + 1) % fm->capacity;
+    fm->size -= 1;
+
+    return data;
 }
--- main.c
+++ main.c
@@ -1,7 +1,33 @@
-#include <stdio.h>
+int main(void) {
+    uint8_t frame_size = 4;
 
-int main(void)
-{
-    printf("Hello World!\n");
+    FrameQueue *fm = framequeue_new(3, frame_size);
+    uint8_t *temp_frame = malloc(sizeof(uint8_t) * frame_size);
+    
+    for (int i = 0; i < 50; i++)
+    {
+        for(int j = 0; j < frame_size; j++){
+            int r = rand() % 250;
+            temp_frame[j] = r;
+        }
+
+        framequeue_enqueue(fm, temp_frame);
+
+        if (i % 2 == 0) {
+            uint8_t * data = framequeue_dequeue(fm);
+            if(data != NULL){
+                puts("\n");
+                for(int j = 0; j < frame_size; j++){
+                    if (j % 10 == 0) {
+                        printf("%02d\n", j);
+                    }
+                    printf("p: %03d ",data[j]);
+                }
+                free(data);
+            }
+        }
+    }
+
+    framequeue_free(fm);
     return 0;
 }
