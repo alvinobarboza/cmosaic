39e6a27bbfae64504a5f0cbc3b36dd54dcc5418b
Alvino Barboza <alvinobarboza@gmail.com>
2025-08-29 11:48:53 -0300

    Formatting

--- framequeue.h
+++ framequeue.h
@@ -9,15 +9,16 @@
 #define MIN_FRAME_QUEUE 3
 
 typedef struct FrameQueue {
-    uint8_t capacity;
-    uint8_t head;
-    uint8_t tail;
-    uint8_t size;
-    uint8_t **queue;
-    uint32_t frame_size;
+    uint8_t     capacity;
+    uint8_t     head;
+    uint8_t     tail;
+    uint8_t     size;
+    uint8_t     **queue;
+    uint32_t    frame_size;
 } FrameQueue;
 
 FrameQueue *framequeue_new(uint8_t buffer_size, uint32_t frame_size);
 void framequeue_free(FrameQueue *fm);
+
 void framequeue_enqueue(FrameQueue *fm, uint8_t *data);
 bool framequeue_dequeue(FrameQueue *fm, uint8_t *data);
