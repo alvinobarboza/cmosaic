ff5586646a01399fc46741b4ada48df8310582f7
Alvino N. C. Barboza <alvinobarboza@gmail.com>
2025-08-31 22:23:27 -0300

    Read current frame from queue and write to Raylib array color canvas relative to its location in the screen

--- videodata.c
+++ videodata.c
@@ -1,19 +1,40 @@
 #include "videodata.h"
 
-VideoData *videdata_new(Sources *s, FrameQueue *fq, uint32_t frame_size) {
+VideoData *videdata_new(Sources *s, FrameQueue *fq,  Vec2 res, Vec2 pos) {
     VideoData *vd = malloc(sizeof(VideoData));
     vd->queue = fq;
     vd->source = s;
-    vd->frame_size = frame_size;
+    vd->posisition = pos;
+    vd->resolution = res;
 
-    vd->current_frame = malloc(sizeof(uint8_t) * frame_size);
+    vd->current_frame_buf = malloc(sizeof(uint8_t) * res.x * res.y * COLORS_CHANNEL);
 
     return vd;
 }
  
 void videodata_free(VideoData *vd) {
     if (vd != NULL) {
-        free(vd->current_frame);
+        free(vd->current_frame_buf);
         free(vd);
+    }
+}
+
+
+void videodata_update_canvas(VideoData *vd, Color *canvas, uint32_t w, uint32_t h) {
+    if (framequeue_dequeue(vd->queue, vd->current_frame_buf)) {
+        for (uint32_t y = 0; y < vd->resolution.y; y++) 
+        {
+            for (uint32_t x = 0; x < vd->resolution.x*COLORS_CHANNEL; x+=3) 
+            {
+                uint32_t index = (y+vd->posisition.y)*w+(x+vd->posisition.x);
+                if (index >= w*h*COLORS_CHANNEL) {
+                    continue;
+                }
+                canvas[index/COLORS_CHANNEL].r = vd->current_frame_buf[index+0];
+                canvas[index/COLORS_CHANNEL].g = vd->current_frame_buf[index+1];
+                canvas[index/COLORS_CHANNEL].b = vd->current_frame_buf[index+2];
+                canvas[index/COLORS_CHANNEL].a = 255;
+            }
+        }        
     }
 }
--- videodata.h
+++ videodata.h
@@ -3,13 +3,26 @@
 #include "configreader.h"
 #include "framequeue.h"
 
+#include "include/raylib/include/raylib.h"
+
+#define COLORS_CHANNEL 3
+
+typedef struct Vec2
+{
+    uint32_t x, y;
+} Vec2;
+
+
 typedef struct VideoData
 {
     Sources *source;
     FrameQueue *queue;
-    uint32_t frame_size;
-    uint8_t *current_frame;
+    Vec2 resolution;
+    Vec2 posisition;
+    uint8_t *current_frame_buf;
 } VideoData;
 
-VideoData *videdata_new(Sources *s, FrameQueue *fq, uint32_t frame_size);
+VideoData *videdata_new(Sources *s, FrameQueue *fq, Vec2 res, Vec2 pos);
 void videodata_free(VideoData *vd);
+
+void videodata_update_canvas(VideoData *vd, Color *canvas, uint32_t w, uint32_t h);
