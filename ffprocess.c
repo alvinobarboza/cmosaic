72fe887d997a63c5ff3d4ace152a9149b96bd377
ALVINO NOEL CONCEIÇÃO BARBOZA <alvinobarboza@gmail.com>
2025-09-05 17:04:38 -0300

    Dynamically load resolution scale and close on request

--- ffprocess.c
+++ ffprocess.c
@@ -5,43 +5,36 @@
 
 #include "framebuffer.h"
 
-char *resolution(enum MType t) {
-    switch (t)
-    {
-    case T_1X1:
-        return "1920:1080";
-    case T_2X2:
-        return "960:540";
-    case T_3X3:
-        return "640:360";
-    default:
-        return "\0";
-    } 
+void resolution(char res[MAX_RES_LENGTH], enum MType t, uint16_t w, uint16_t h) {
+    snprintf(res, 12, "%d:%d", w/t, h/t);
 }
 
 void make_cmd(char *cmd, const char *input, const char *resolution) {
-    snprintf(cmd, MAX_CMD_LENGTH, "ffmpeg -hide_banner -re -i '%s' -filter_complex '[0:v:0]scale=%s:flags=fast_bilinear,fps=fps=15,format=rgb24[vi]' -map '[vi]' -f rawvideo pipe:1", input, resolution);
+    snprintf(cmd, MAX_CMD_LENGTH, "ffmpeg -hide_banner -re -stream_loop -1 -i '%s' -filter_complex '[0:v:0]scale=%s:flags=fast_bilinear,fps=fps=15,format=rgb24[vi]' -map '[vi]' -f rawvideo pipe:1", input, resolution);
 }
 
 void * init_ff_process(void * arguments) {
-    pThreadArgs args = *(pThreadArgs *) arguments;
+    pThreadArgs *args = (pThreadArgs *) arguments;
 
     FILE *fp;
     int16_t charout;
 
     char command[MAX_CMD_LENGTH];
-    char *res = resolution(args.type);
-    make_cmd(command, args.s->source, res);
+    char res[MAX_RES_LENGTH];
+    resolution(res, args->type, args->screen_width, args->screen_height);
+
+    make_cmd(command, args->s->source, res);
 
     fp = popen(command, "r");
     if (fp==NULL){
-        puts("Failed to start ffmpeg process!");
+        perror("Failed to start ffmpeg process!");
         pthread_exit(NULL);
         return 0;
     }
 
-    while((charout=fgetc(fp))!=EOF) {
-        framebuffer_write_data(args.fb, charout);
+    while((charout=fgetc(fp))!=EOF ) {
+        framebuffer_write_data(args->fb, charout);
+        if (args->close) break;
     }
 
     pclose(fp);
--- ffprocess.h
+++ ffprocess.h
@@ -4,12 +4,16 @@
 #include "framebuffer.h"
 #include "configreader.h"
 
+#define MAX_RES_LENGTH 12
 #define MAX_CMD_LENGTH 500
 
 typedef struct pThreadArgs {
     FrameBuffer *fb;
     Sources *s;
     enum MType type;
+    uint16_t screen_width;
+    uint16_t screen_height;
+    bool close;
 } pThreadArgs;
 
 void * init_ff_process(void * args);
